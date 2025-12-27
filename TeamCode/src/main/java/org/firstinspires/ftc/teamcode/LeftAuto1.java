bb194dc374d6415eea6e29f2db358a5a156c5c23
Maharoga Save me <166644817+JJKchapter236@users.noreply.github.com>
2025-12-27 16:55:46 -0500

    Update LeftAuto1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto1.java
@@ -23,13 +23,13 @@ public class LeftAuto1 extends OpMode {
     private ElapsedTime pathTimer, actionTimer, opmodeTimer;
     private final Pose startPose = new Pose(20.639209225700164, 122.41186161449752, Math.toRadians(136)); // Start Pose of our robot.
     private final Pose scorePose = new Pose(61.680395387149915, 81.37067545304778, Math.toRadians(136)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
-    private final Pose pickup1Pose = new Pose(13.28500823723229, 83.98023064250413, Math.toRadians(180)); // Highest (First Set) of Artifacts from the Spike Mark.
-    private final Pose pickup2Pose = new Pose(11.387149917627676, 60.019769357495896, Math.toRadians(180)); // Middle (Second Set) of Artifacts from the Spike Mark.
-    private final Pose pickup3Pose = new Pose(11.387149917627676, 34.87314662273476, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
+    private final Pose pickup1Pose = new Pose(17, 83.98023064250413, Math.toRadians(180)); // Highest (First Set) of Artifacts from the Spike Mark.
+    private final Pose pickup2Pose = new Pose(17, 60.019769357495896, Math.toRadians(180)); // Middle (Second Set) of Artifacts from the Spike Mark.
+    private final Pose pickup3Pose = new Pose(17, 34.87314662273476, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
     private final Pose rotation1Pose = new Pose(49.344316309719936, 84.2174629324547, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
     private final Pose rotation2Pose = new Pose(41.990115321252055, 59.78253706754529, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
     private final Pose rotation3Pose = new Pose(41.51565074135091, 35.11037891268535, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
-    private final Pose finalPose = new Pose(55.98682042833608, 64.05271828665569, Math.toRadians(90)); // Lowest (Third Set) of Artifacts from the Spike Mark.
+    private final Pose finalPose = new Pose(35, 64.05271828665569, Math.toRadians(90)); // Lowest (Third Set) of Artifacts from the Spike Mark.
     private PathChain rotate1, grabPickup1, shootPickup1, rotate2, grabPickup2, shootPickup2, rotate3, grabPickup3, shootPickup3,finalPosition;
     private Path scorePreload;
     public void buildPaths(){
