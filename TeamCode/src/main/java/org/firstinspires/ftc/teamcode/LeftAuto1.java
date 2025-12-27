4c3710992a79ac353d57b753ee4a21f196128c8e
Maharoga Save me <166644817+JJKchapter236@users.noreply.github.com>
2025-12-27 16:30:11 -0500

    Update LeftAuto1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto1.java
@@ -3,15 +3,24 @@ package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
 
+import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import java.util.Timer;
 
 @Autonomous
-public class OneDayAfterXMasAutoTest extends OpMode {
+public class LeftAuto1 extends OpMode {
+    private int pathState;
+    private Follower follower;
+    private ElapsedTime pathTimer, actionTimer, opmodeTimer;
     private final Pose startPose = new Pose(20.639209225700164, 122.41186161449752, Math.toRadians(136)); // Start Pose of our robot.
     private final Pose scorePose = new Pose(61.680395387149915, 81.37067545304778, Math.toRadians(136)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     private final Pose pickup1Pose = new Pose(13.28500823723229, 83.98023064250413, Math.toRadians(180)); // Highest (First Set) of Artifacts from the Spike Mark.
@@ -21,7 +30,7 @@ public class OneDayAfterXMasAutoTest extends OpMode {
     private final Pose rotation2Pose = new Pose(41.990115321252055, 59.78253706754529, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
     private final Pose rotation3Pose = new Pose(41.51565074135091, 35.11037891268535, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
     private final Pose finalPose = new Pose(55.98682042833608, 64.05271828665569, Math.toRadians(90)); // Lowest (Third Set) of Artifacts from the Spike Mark.
-    private PathChain rotate1, grabPickup1, shootPickup1, rotate2, grabPickup2, shootPickup2, rotate3, grabPickup3, shootPickup3;
+    private PathChain rotate1, grabPickup1, shootPickup1, rotate2, grabPickup2, shootPickup2, rotate3, grabPickup3, shootPickup3,finalPosition;
     private Path scorePreload;
     public void buildPaths(){
         scorePreload = new Path(new BezierLine(startPose, scorePose));
@@ -62,14 +71,126 @@ public class OneDayAfterXMasAutoTest extends OpMode {
                 .addPath(new BezierLine(pickup3Pose, scorePose))
                 .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose.getHeading())
                 .build();
+        finalPosition= follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, finalPose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), finalPose.getHeading())
+                .build();
     }
+    public void setPathState(int pState) {
+        pathState = pState;
+    }
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePreload);
+                setPathState(1);
+                break;
+            case 1:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(rotate1, true);
+                    setPathState(2);
+                }
+                break;
+            case 2:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(grabPickup1, true);
+                    setPathState(3);
+                }
+                break;
+            case 3:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(shootPickup1, true);
+                    setPathState(4);
+                }
+                break;
+            case 4:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(rotate2, true);
+                    setPathState(5);
+                }
+                break;
+            case 5:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(grabPickup2, true);
+                    setPathState(6);
+                }
+                break;
+            case 6:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(shootPickup2, true);
+                    setPathState(7);
+                }
+                break;
+            case 7:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(rotate3, true);
+                    setPathState(8);
+                }
+                break;
+            case 8:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(grabPickup3, true);
+                    setPathState(9);
+                }
+                break;
+            case 9:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(shootPickup3, true);
+                    setPathState(10);
+                }
+                break;
+            case 10:
+
+                if (!follower.isBusy()) {
+
+                    follower.followPath(finalPosition, true);
+                    setPathState(-1);
+                }
+                break;
+        }
+    }
+
     @Override
     public void init() {
-
+        opmodeTimer = new ElapsedTime();
+        opmodeTimer.reset();
+        follower = Constants.createFollower(hardwareMap);
+        buildPaths();
+        follower.setStartingPose(startPose);
     }
 
     @Override
     public void loop() {
 
+        follower.update();
+        autonomousPathUpdate();
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+    @Override
+    public void start() {
+        opmodeTimer.reset();
+        setPathState(0);
     }
 }
