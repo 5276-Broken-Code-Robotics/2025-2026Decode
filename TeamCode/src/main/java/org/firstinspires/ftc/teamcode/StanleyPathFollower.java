6fd891a7b784454ab584ba6398778e4cb44b9c03
Stanley Liu <152922311+javameitser@users.noreply.github.com>
2025-12-28 10:48:00 -0500

    added testing for path following

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StanleyPathFollower.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.paths.StanleyAutoP1;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+
+
+@Autonomous
+public class StanleyPathFollower extends OpMode {
+
+    StanleyAutoP1 path1;
+
+    Follower follower;
+    int dex = 0;
+
+    @Override
+    public void init() {
+
+        follower = Constants.createFollower(hardwareMap);
+        path1 = new StanleyAutoP1(follower);
+        dex = 0;
+    }
+
+    @Override
+    public void loop() {
+
+        follower.update();
+        if(!follower.isBusy() && dex < path1.paths.size()){
+            follower.followPath(path1.paths.get(dex));
+            dex++;
+
+        }
+
+        telemetry.addData("Path Index", dex);
+        telemetry.addData("Busy", follower.isBusy());
+        telemetry.update();
+    }
+}
