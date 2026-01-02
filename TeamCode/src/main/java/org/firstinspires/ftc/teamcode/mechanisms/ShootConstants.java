e083293ef9ebf4bc9250502fc682c58daa59e020
Ethan Filip <ethie.alexandru@gmail.com>
2026-01-02 17:46:42 -0500

    update power and tilt fromDistance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ShootConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ShootConstants.java
@@ -5,28 +5,19 @@ public class ShootConstants {
     public static double aprilTagMoveScanTimePerStep_seconds = 0.4;
     public static double flywheelAccelerationTime_seconds = 1;
     public static double shotDuration_seconds = 2.5;
-
-
-    static double sizePerDistanceSegment_inches = 144d / 3;
     
     public static double powerFromDistance(double distance) {
-        if(distance <= sizePerDistanceSegment_inches) {
-            return 0.65;
-        } else if (distance <= sizePerDistanceSegment_inches * 2) {
-            return 0.75;
+        if(distance <= 3) {
+            return 0.575;
+        } else if (distance <= 6) {
+            return 0.63;
         } else {
-            return 0.8;
+            return 0.70;
         }
     }
 
 
     public static double tiltFromDistance(double distance){
-        if(distance <= sizePerDistanceSegment_inches) {
-            return 0;
-        } else if (distance <= sizePerDistanceSegment_inches * 2) {
-            return 0;
-        } else {
-            return 0;
-        }
+        return 0;
     }
 }
