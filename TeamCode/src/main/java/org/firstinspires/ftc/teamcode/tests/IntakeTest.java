83700b9587988f80417de30ec31a0682cb43c81e
Ethan Filip <ethie.alexandru@gmail.com>
2026-02-26 16:47:44 -0500

    organization, delete old
    -organized all code files in teamcode into auto red and blue, teleop, and tests folders
    -added correct group for all teleop and auto
    -deleted obsolete files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/IntakeTest.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.mechanisms.POVDrive;
+
+@TeleOp(group = "Tests")
+public class IntakeTest extends OpMode {
+
+    POVDrive drive = new POVDrive();
+    DcMotor intake;
+
+
+    @Override
+    public void init() {
+
+        drive.init(hardwareMap);
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+    }
+
+    @Override
+    public void loop() {
+
+        drive.drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        intake.setPower(1);
+
+
+    }
+}
