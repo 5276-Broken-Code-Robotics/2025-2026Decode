08cfc9f1fa3c384087ba7fd4de6e8e6ca216bd9f
Abraham klein <166644817+JJKchapter236@users.noreply.github.com>
2026-03-12 00:25:33 -0400

    done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/PIDFTUNING.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/PIDFTUNING.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.tests;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,7 +13,7 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 public class PIDFTUNING {
     public DcMotorEx flywheel1;
     double closeRange=600;
-    double midRange=1200;
+
     double longRange=1800;
     //the values used here are temporary or starting values depending on how you want to look at it
     double currentTarget;
@@ -19,6 +21,7 @@ public class PIDFTUNING {
     double P=0;
 
     double[] stepSizes={10.0,1.0,0.1,0.01,0.001};
+    int stepIndex=1;
 
 
     public void init(){
@@ -29,6 +32,38 @@ public class PIDFTUNING {
         flywheel1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, goonCoefficients);
     }
     public void loop(){
-
+        if (gamepad1.yWasPressed()){
+            if (currentTarget==longRange){
+                currentTarget=closeRange;
+            }else{
+                currentTarget=longRange;
+            }
+        }
+        if (gamepad1.bWasPressed()){
+            stepIndex=(stepIndex+1)%stepSizes.length;
+        }
+        if(gamepad1.dpadLeftWasPressed()){
+            F+=stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadRightWasPressed()){
+            F+=stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadUpWasPressed()){
+            P+=stepSizes[stepIndex];
+        }
+        if (gamepad1.dpadDownWasPressed()){
+            P-=stepSizes[stepIndex];
+        }
+        flywheel1.setVelocity(currentTarget);
+        double currentVelocity=flywheel1.getVelocity();
+        double error=currentTarget-currentVelocity;
+        telemetry.addData("Target Velocity", currentTarget);
+        telemetry.addData("Current Velocity", "%.2f", currentVelocity);
+        telemetry.addData("Error", "%.2f", error);
+        telemetry.addLine("------------------------------");
+        telemetry.addData("Tuning P", "%.4f (D-Pad )", P);
+        telemetry.addData("Tuning F", "%.4f (D-Pad L/R)", F);
+        telemetry.addData("Step Size", "%.4f (B Button)", stepSizes[stepIndex]);
+        telemetry.update();
     }
 }
