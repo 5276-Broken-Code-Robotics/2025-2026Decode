e460f0207a73c238dac225e9f268c446031bb3fe
Eli <82536604+Fudgeel@users.noreply.github.com>
2026-03-12 20:47:56 -0400

    fahh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PanTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PanTracking.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class PanTracking{
     DcMotor pan;
     double tAngle;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ObolistkDetechtionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ObolistkDetechtionTest.java
@@ -6,22 +6,37 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.tel
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.util.List;
+import java.util.jar.Attributes;
 
 
-public class ObolistkDetechtionTest {
+@Autonomous(group = "Tests")
+public class ObolistkDetechtionTest extends OpMode {
     Limelight3A limelight;
     int obeliskId = 0;
-    public void init(){
+
+    DcMotor pan;
+
+    public void init() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100);
         limelight.start();
         limelight.pipelineSwitch(2);
+
+        pan = hardwareMap.get(DcMotor.class, "rot");
+        pan.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pan.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER5);
     }
+
     public void loop() {
         LLResult result = limelight.getLatestResult();
-        if (obeliskId == 2) {
+        telemetry.addData("obeliskId", obeliskId);
+
             List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
             for (LLResultTypes.FiducialResult fiducial : fiducials) {
                 obeliskId = fiducial.getFiducialId();
@@ -43,9 +58,10 @@ public class ObolistkDetechtionTest {
             if (obeliskId == 23) {
                 char[] pattern = {'p', 'p', 'g'};
             }
-        }
-        }
-    public void start(){
+
+            telemetry.addData("Pos:", pan.getCurrentPosition());
+
 
     }
-    }
+}
+
