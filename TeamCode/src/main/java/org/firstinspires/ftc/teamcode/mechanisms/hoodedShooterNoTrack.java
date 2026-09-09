685f0825b06809488f03c0d16db62cee7ed8606c
Stanley Liu <corruptedpig29@gmail.com>
2026-09-09 11:44:21 -0400

    made showcase

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/hoodedShooterNoTrack.java
@@ -0,0 +1,339 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+public class hoodedShooterNoTrack {
+
+    public FinalFreeSortHSV freeSort;
+
+
+    private Servo tilt;
+
+    ElapsedTime shootTimer;
+
+
+    public char p1;
+    public char p2;
+    public char p3;
+
+
+    int id = 0;
+    ElapsedTime elapsedTime;
+
+
+
+
+
+
+    public boolean shotbegan;
+    private DcMotor flywheel1;
+
+    private DcMotor flywheel2;
+
+
+
+    //0.75 for close sec 2
+
+    public double flywheelPower;
+
+    public double headingTiltPos;
+    Telemetry telemetry;
+
+
+    boolean resetting = false;
+
+    ElapsedTime waitrotate;
+
+    double angleToAprilTag = 0;
+    Pose2D aprilTagRed = new Pose2D(DistanceUnit.INCH,144,144, AngleUnit.RADIANS,0);
+    Pose2D aprilTagBlue = new Pose2D(DistanceUnit.INCH,0,144, AngleUnit.RADIANS,0);
+    boolean rotated = false;
+    Pose2D currentAprilTagPos;
+
+
+    int lastOrientedPos;
+    boolean isAutoShot;
+
+    Pose initPose;
+    int currentID;
+    Limelight3A limelight;
+
+    GoBildaPinpointDriver pinpoint;
+
+    public ElapsedTime pinpointUpdatePause;
+    // (160 (vbig gear) / 18 (small gear)  * 20 (degrees of rotation))/300
+    double maxTilt = 0.59;
+    public void init(HardwareMap hardwareMap, Telemetry telemetry, GoBildaPinpointDriver pinpoint, int id) {
+
+
+        shootTimer = new ElapsedTime();
+
+        freeSort = new FinalFreeSortHSV();
+
+        freeSort.init(hardwareMap, telemetry);
+
+        shootTimer.reset();
+
+        pinpointUpdatePause = new ElapsedTime();
+        tilt = hardwareMap.get(Servo.class, "tilt");
+        flywheel1 = hardwareMap.get(DcMotor.class, "flywheel1");
+        flywheel2 = hardwareMap.get(DcMotor.class, "flywheel2");
+        limelight = hardwareMap.get(Limelight3A.class,"limelight");
+
+
+        this.id = id;
+
+
+        this.pinpoint = pinpoint;
+
+
+        flywheel1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        flywheel2.setDirection(DcMotorSimple.Direction.FORWARD);
+        flywheel1.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitrotate = new ElapsedTime();
+        resetting = false;
+
+
+
+
+        flywheelPower = 0.6;
+        headingTiltPos = 0.12;
+
+
+        if(id == 24){
+            currentAprilTagPos = aprilTagRed;
+        }else{
+            currentAprilTagPos = aprilTagBlue;
+        }
+
+
+        currentID = id;
+
+
+
+
+        //limelight.start();
+        //DISABLED FOR POWER PURPOSES, DO NOT FORGET TO REENABLE
+
+
+        this.telemetry = telemetry;
+
+
+
+
+        elapsedTime = new ElapsedTime();
+    }
+
+    public void start(){
+        pinpointUpdatePause.reset();
+        panTracking.start();
+
+    }
+
+    public void loop()
+    {
+
+
+
+        telemetry.addData("dex for the firing loop", dex);
+
+        telemetry.addData("Pattern Arr : ", curpat[0]+ " , ", curpat[1], " , ", curpat[2]);
+
+
+        if(id == 24){
+            currentAprilTagPos = aprilTagRed;
+        }else{
+            currentAprilTagPos = aprilTagBlue;
+        }
+
+
+        //Alter, this is just for red goal
+        if((currentAprilTagPos.getY(DistanceUnit.INCH)-pinpoint.getPosY(DistanceUnit.INCH)) * (currentAprilTagPos.getY(DistanceUnit.INCH)-pinpoint.getPosY(DistanceUnit.INCH)) + (currentAprilTagPos.getX(DistanceUnit.INCH)-pinpoint.getPosX(DistanceUnit.INCH))*(currentAprilTagPos.getX(DistanceUnit.INCH)-pinpoint.getPosX(DistanceUnit.INCH)) < 3800 ){
+            headingTiltPos = 0.051;
+        }else if(pinpoint.getPosY(DistanceUnit.INCH) <= 36) {
+            headingTiltPos = 0.075;
+        }else{
+            headingTiltPos = 0.1;
+        }
+
+
+        freeSort.loop();
+
+        //Finish the results script
+
+
+
+
+        if(firingPat)FiringAPattern();
+
+        p1 = freeSort.pos1;
+        p2 = freeSort.pos2;
+        p3 = freeSort.pos3;
+
+        /*
+
+
+        telemetry.addData("Flywheel power", flywheel1.getPower() + " " + flywheel2.getPower());
+        telemetry.addData("Pan position : ", pan.getCurrentPosition());
+
+
+
+         */
+
+        if(pinpointUpdatePause.seconds() > 0.3)pinpoint.update();
+
+
+
+        //Orienting();
+
+
+
+
+        flywheel1.setPower(flywheelPower);
+        flywheel2.setPower(flywheelPower);
+
+        //telemetry.update();
+    }
+
+
+
+    public void AutoBeginShot(boolean b, boolean a){
+
+    }
+
+
+
+    public boolean firingPat = false;
+
+
+    char[] curpat = {'p', 'p', 'p'};
+
+    public void firePattern(){
+        firingPat = true;
+        dex = 0;
+        shootTimer.reset();
+    }
+
+
+
+
+    int dex = 0;
+
+    public void FiringAPattern(){
+        if(dex < 3){
+            if(shootTimer.seconds() > freeSort.shootDur+0.2){
+                if(dex < curpat.length)FireColor(curpat[dex]);
+                dex++;
+                shootTimer.reset();
+            }
+        }else{
+            if(freeSort.pos1 == 'e' && freeSort.pos2 == 'e' && freeSort.pos3 == 'e') {
+                firingPat = false;
+                dex = 0;
+            }else{
+                firingPat = true;
+                dex = 0;
+            }
+        }
+
+    }
+
+
+    public PanTracking panTracking;
+
+
+
+    public void Fire(int num){
+        if(!freeSort.shooting)freeSort.shoot(num);
+    }
+
+    public void FireColor(char c){
+
+        if(c == 'p'){
+            if(freeSort.pos1 == 'p'){
+                freeSort.shoot(1);
+            }else
+            if(freeSort.pos2 == 'p'){
+                freeSort.shoot(2);
+            }else
+            if(freeSort.pos3 == 'p'){
+                freeSort.shoot(3);
+            }else{
+                if(freeSort.pos1 == 'g'){
+                    freeSort.shoot(1);
+                }else
+                if(freeSort.pos2 == 'g'){
+                    freeSort.shoot(2);
+                }else
+                if(freeSort.pos3 == 'g'){
+                    freeSort.shoot(3);
+                }
+            }
+
+        }else{
+            if(freeSort.pos1 == 'g'){
+                freeSort.shoot(1);
+            }else
+            if(freeSort.pos2 == 'g'){
+                freeSort.shoot(2);
+            }else
+            if(freeSort.pos3 == 'g'){
+                freeSort.shoot(3);
+            }else{
+                if(freeSort.pos1 == 'p'){
+                    freeSort.shoot(1);
+                }else
+                if(freeSort.pos2 == 'p'){
+                    freeSort.shoot(2);
+                }else
+                if(freeSort.pos3 == 'p'){
+                    freeSort.shoot(3);
+                }
+            }
+        }
+
+
+
+    }
+
+    public static double turnAngle(double currentHeading, double targetAngle) {
+        double error = targetAngle - currentHeading;
+
+        // Normalize to (-PI, PI]
+        while (error > Math.PI)  error -= 2 * Math.PI;
+        while (error <= -Math.PI) error += 2 * Math.PI;
+
+        return error;
+    }
+
+    public static double distTo(Pose tagPos, Pose ourPos){
+        return Math.sqrt((ourPos.getY() - tagPos.getY())*(ourPos.getY() - tagPos.getY()) + (ourPos.getX() - tagPos.getX())*(ourPos.getX() - tagPos.getX()));
+    }
+
+    public static double AngleToTag(Pose tagPos, Pose ourPos){
+        return Math.atan2(tagPos.getY() - ourPos.getY(), tagPos.getX() - ourPos.getX());
+    }
+
+
+}
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/showcaseTeleOp.java
@@ -0,0 +1,256 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.mechanisms.FieldRelativeDrive;
+import org.firstinspires.ftc.teamcode.mechanisms.HoodedShooter;
+import org.firstinspires.ftc.teamcode.mechanisms.hoodedShooterNoTrack;
+
+@TeleOp(group = "TeleOp")
+
+
+
+public class showcaseTeleOp extends OpMode {
+    boolean beganshot = false;
+
+    double initpos = 0f;
+
+    private DcMotor fr;
+
+    private DcMotor fl;
+
+    private DcMotor br;
+    private DcMotor bl;
+
+
+
+
+    FieldRelativeDrive fieldDrive;
+    ElapsedTime elapsedTime;
+
+    private DcMotor intake;
+
+
+
+
+
+    int state = 0;
+
+    //0 : looking for
+    //1 : orienting correctly
+
+
+    ElapsedTime timer;
+
+
+
+    double positionnecessary = 0f;
+
+
+
+
+    Pose aprilTagRed = new Pose(144,144+15);
+    Pose aprilTagBlue = new Pose(0,144+15);
+
+    hoodedShooterNoTrack hShooter;
+
+    ElapsedTime rewindTimer;
+
+    GoBildaPinpointDriver pinpoint;
+
+
+    Servo arm1;
+
+    Limelight3A limelight;
+    boolean rewinding = false;
+    int num = 0;
+    public void init() {
+
+        rewindTimer = new ElapsedTime();
+        rewindTimer.reset();
+
+        num = 0;
+
+//        follower = Constants.createFollower(hardwareMap);
+//
+//        follower.setPose(new Pose(0,144));
+
+
+        fieldDrive = new FieldRelativeDrive();
+
+
+
+
+
+
+
+
+        timer = new ElapsedTime();
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+        br = hardwareMap.get(DcMotor.class, "br");
+
+
+        arm1 = hardwareMap.get(Servo.class, "arm1");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+
+        limelight = hardwareMap.get(Limelight3A.class,"limelight");
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class,"pinpoint");
+
+
+
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED,
+                GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        //pinpoint.resetPosAndIMU();
+
+
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        //intake.setDirection(DcMotor.Direction.REVERSE);
+
+
+        timer.reset();
+        beganshot = false;
+        elapsedTime = new ElapsedTime();
+
+        elapsedTime.reset();
+
+        hShooter = new hoodedShooterNoTrack();
+
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        hShooter.init(hardwareMap,telemetry, pinpoint, 24);
+
+        state=  0;
+        intake.setPower(0);
+
+        fieldDrive.init(hardwareMap, gamepad1, pinpoint);
+
+
+    }
+
+    @Override
+    public void start(){
+        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH,96, 8.5, AngleUnit.RADIANS,0));
+
+        hShooter.start();
+        intake.setPower(1);
+
+
+
+    }
+    double val = 0.6;
+
+    public void breaks(){
+    }
+
+
+    public void loop(){
+
+
+
+
+
+        if(gamepad2.a){
+            //rewindTimer.reset();
+            if(intake.getPower() == 1 )intake.setPower(-1);
+
+
+            //rewinding = true;
+        }else{
+            if(intake.getPower() == -1)intake.setPower(1);
+        }
+
+
+
+        if(gamepad2.leftBumperWasPressed()){
+            hShooter.FireColor('p');
+        }
+        if(gamepad2.rightBumperWasPressed()){
+            hShooter.FireColor('g');
+        }
+
+        if(gamepad2.dpadDownWasPressed()){
+
+            telemetry.addData("We are on square", "hooray");
+            hShooter.Fire(1);
+        }
+        if(gamepad2.dpadRightWasPressed()){
+
+            telemetry.addData("We are on circle", "Wow");
+            hShooter.Fire(2);
+        }
+
+        if(gamepad2.dpadLeftWasPressed()){
+
+            telemetry.addData("We are on triangle", "Wow");
+            hShooter.Fire(3);
+        }
+
+
+        if(gamepad2.right_trigger > 0  && !hShooter.firingPat){
+            hShooter.firePattern();
+        }
+
+
+
+        if(gamepad2.y){
+            fl.setPower(0.0);
+            fr.setPower(0.0);
+            br.setPower(0.0);
+            bl.setPower(0.0);
+        }else{
+            fieldDrive.driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            if(gamepad1.left_stick_y == 0 && gamepad1.left_stick_x == 0 && gamepad1.right_stick_x == 0)fieldDrive.driveFieldRelative(-gamepad2.left_stick_y/4, gamepad2.left_stick_x/4, gamepad2.right_stick_x/4);
+
+        }
+
+
+
+
+
+
+
+        if(gamepad2.dpadUpWasPressed()){
+
+            hShooter.firePattern();
+        }
+
+        if(gamepad1.dpadUpWasPressed()){
+            pinpoint.setHeading(0,AngleUnit.RADIANS);
+        }
+
+        hShooter.loop();
+
+
+
+        telemetry.addData("According to redslave 1 we are at : ", pinpoint.getPosX(DistanceUnit.INCH) + " " + pinpoint.getPosY(DistanceUnit.INCH) + " At an angle of " +pinpoint.getHeading(AngleUnit.RADIANS));
+        //telemetry.update();
+
+        telemetry.addData("Positions : ", hShooter.p1 + " , " + hShooter.p2 + " , " + hShooter.p3);
+
+
+        telemetry.update();
+    }
+
+
+}
